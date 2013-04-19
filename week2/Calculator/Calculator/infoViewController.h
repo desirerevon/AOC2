2fa14581bd104fc4c70c953e3dd7fb28728a0c8e
desirerevon <desirerevon@fullsail.edu>
2013-04-18 23:26:20 -0500

    commiting random file to week1

---
+++ week2/Calculator/Calculator/infoViewController.h
@@ -0,0 +1,13 @@
+//
+//  infoViewController.h
+//  Calculator
+//
+//  Created by Desire Gardner on 4/18/13.
+//  Copyright (c) 2013 Desire Gardner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface infoViewController : NSObject
+
+@end
---
+++ week2/testApp/testApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testApp
+//
+//  Created by Desire Gardner on 4/18/13.
+//  Copyright (c) 2013 Desire Gardner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
